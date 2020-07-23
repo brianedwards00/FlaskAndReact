5f8d60d2810cdab543b3a1a5c462a116e7362e22
brianedwards00 <brian_edwards@csu.fullerton.edu>
2020-07-23 15:48:04 -0700

    Started adding CSS styles

--- FlaskAndReact/react-flask-app/src/App.js
+++ FlaskAndReact/react-flask-app/src/App.js
@@ -3,6 +3,7 @@ import MultiLevelSelect from 'react-multi-level-selector'
 import './App.css';
 import JSZip from 'jszip';
 
+// Maybe add a Start Over button that refreshes page
 class App extends React.Component {
 
     constructor(props) {
@@ -14,7 +15,7 @@ class App extends React.Component {
             selectedFiles: null,
             options: [{value: 'data', label: 'data', options: []},
                 {value: 'plots', label: 'plots', options: []}],
-            optionsChosen: [],
+            optionsChosen: [], file_data: []
         }
 
         this.handleChange = this.handleChange.bind(this)
@@ -83,7 +84,8 @@ class App extends React.Component {
             return
         }
         document.getElementById('loading').innerHTML = "Loading..."
-        let read_zip = new JSZip()
+        document.getElementById('submit_button2').disabled = true
+        let filedata = []
         fetch(
             "http://localhost:5000/dropdown_submit", {
                 method: 'POST',
@@ -100,10 +102,33 @@ class App extends React.Component {
                     return Promise.reject(new Error(response.statusText));
                 }
             })
-            .then(results_zip => read_zip.loadAsync(results_zip))
-            .then(function (zip) {
-                console.log(zip.files)
+            .then(JSZip.loadAsync)
+            .then(function(zip) {
+                // console.log(zip.file().)
+                console.log(zip.files,"First")
+                console.log(zip,'+++++',typeof zip)
+                for (let index in zip.files) {
+                    console.log('INDEX',index)
+                    console.log('STRING',zip.file(index).async("string"))
+                    console.log('ARRAY',zip.file(index).async("arraybuffer"))
+                    //return zip.files[index].async('string')
+                    return ""
+        }
+        //FIXME: promise failure, want to get back!
+        //TODO: finsied at, tpi
             })
+            .then((text) => {
+                this.setState({file_data: text})
+
+            })
+            .then(() => document.getElementById('area2').style.visibility = 'hidden')
+            .then(() => document.getElementById('area3').style.visibility = 'visible')
+            .then(() => console.log(this.state))
+            .then(() => document.getElementById('data').innerHTML = this.state.file_data)
+            .catch((error) => {
+                console.error('Error:', error)
+            })
+
 }
 
     handleSubmit = (event) => {
@@ -135,7 +160,10 @@ class App extends React.Component {
         let obj = {}
         obj.dataset_name = this.state.dataset_name
         obj.dataset_id = this.state.dataset_id
-
+        document.getElementById('submit_button1').disabled = true
+        document.getElementById('inputName').disabled = true
+        document.getElementById('inputId').disabled = true
+        document.getElementById('inputFiles').disabled = true
         // Uses a POST request to send obj as a JSON string
         document.getElementById('loading_or_fail').innerHTML = "Loading..."
         fetch(
@@ -153,7 +181,7 @@ class App extends React.Component {
             .catch((error) => {
                 console.error('Error:', error)
             })
-        // Uses a POST request to send the FormDatav containing the files
+        // Uses a POST request to send the FormData containing the files
         fetch(
             "http://localhost:5000/files", {
                 method: 'POST',
@@ -164,25 +192,34 @@ class App extends React.Component {
                 console.log('Success:', fileData)
                 fetch("http://localhost:5000/dropdown").then(response => response.text())
                     .then(text => this.handleDropDownChoice(event,text))
-                    .then(a => document.getElementById('success').innerHTML = "Success! Now please select " +
+                    .then(() => document.getElementById('success').innerHTML = "Success! Now please select " +
                     "the csv/png files that you would like to see:")
-                    .then(b => document.getElementById('area2').style.visibility = 'visible')
-                    .then(c => document.getElementById('area1').style.visibility = 'hidden')
+                    .then(() => document.getElementById('area1').style.visibility = 'hidden')
+                    .then(() => document.getElementById('area2').style.visibility = 'visible')
+
             })
             .catch((error) => {
                 console.error('Error:', error)
                 document.getElementById('loading_or_fail').innerHTML = "Error, " +
                     "please make sure 'results.json' is in json format or empty and try again"
+                document.getElementById('submit_button1').disabled = false
             })
     }
     
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit} id = "area1" className={"area1"} style={{'border': 'solid'}}>
+                <header style={{"display" :"block","textAlign":"left","background":"#1C2C58"}}>
+                    <p style={{"display" :"inline-block","textAlign":"center", "paddingLeft":"10px",
+                        "font":"30px Arial Black","margin":"0px","color":"#F2ECE1"}}>Proteomics </p>
+                    <p  className="button" style={{"display" :"inline-block","textAlign":"center","paddingLeft":"50px", "font":"15px Gill Sans","margin":"0px","color":"#F2ECE1"}}>Input</p>
+                    <p style={{"display" :"inline-block","textAlign":"center","paddingLeft":"60px","font":"15px Gill Sans","margin":"0px","color":"#F2ECE1"}}>Output</p>
+                </header>
+                <form onSubmit={this.handleSubmit} id = "area1" className={"area1"}>
                     <label>
-                        <p style={{'fontSize': '20px'}}>Enter the Dataset Name</p>
-                        <input type="text"
+                        <p style={{'font':'20px Comic Sans MS','margin':"0",'padding':'5px','lineHeight':'40px'}}>Enter the Dataset Name</p>
+                        <input
+                               type="text"
                                name="dataset_name"
                                id="inputName"
                                onChange={this.handleChange}
@@ -192,11 +229,11 @@ class App extends React.Component {
                                required
                         />
 
-                        <p style={{'fontSize': '10px'}}>Must only include letters, numbers, underscores ( _ ),
+                        <p style={{'font':'12px Times New Roman','margin':"0",'padding':'5px'}}>Must only include letters, numbers, underscores ( _ ),
                             and/or hyphens ( - )</p>
                     </label>
                     <label>
-                        <p style={{'fontSize': '20px'}}>Enter the Dataset ID</p>
+                        <p style={{'font':'20px Comic Sans MS','margin':"3px",'lineHeight':'40px'}}>Enter the Dataset ID</p>
                         <input type="text"
                                name="dataset_id"
                                id="inputId"
@@ -206,9 +243,9 @@ class App extends React.Component {
                                title="0-9"
                                required
                         />
-                        <p style={{'fontSize': '10px'}}>Must only include integers 0-9, no decimal points</p>
+                        <p style={{'font':'12px Times New Roman','margin':"0",'padding':'5px'}}>Must only include integers 0-9, no decimal points</p>
                     </label>
-                    <label>
+                    <label style={{'font':'20px Comic Sans MS','margin':"0",'padding':'5px','lineHeight':'40px'}}>
                         Please select up to 4 files:
                         <input type="file"
                                className="custom_upload"
@@ -218,30 +255,38 @@ class App extends React.Component {
                                required
                                title=".txt .fasta. or .raw"
                         />
+                        <p style={{'font':'12px Times New Roman','margin':"0",'padding':'5px'}}>Only upload parameter files[.txt], protein files[.fasta]
+                    and mass spectrometer output files[.raw]</p>
+
 
                     </label>
-                    <input style={{'fontSize': '20px'}}
+                    <input style={{'fontSize': '20px','position':'relative','top':'10px'}}
+                           id = "submit_button1"
                            type="submit"
                            value="Submit"
                     />
-                    <p id = "loading_or_fail"/>
+                    <p style={{'font':'20px Comic Sans MS','margin':"0",'padding':'15px'}} id = "loading_or_fail"/>
                 </form>
 
                 <form style={{'border': 'solid'}} onSubmit={this.handleSecondSubmit} id="area2" className={"area2"}>
-                    <p id="success"/>
+                    <p style={{'font':'20px Comic Sans MS','margin':"0",'padding':'5px'}}
+                        id="success"/>
                     <MultiLevelSelect
                     id = "select"
                     options={this.state.options}
                     name = "optionsChosen"
                     onChange = {this.handleDropDownChange}
                     />
-                    <input style={{'fontSize': '20px'}}
+                    <input style={{'fontSize': '20px','position':'relative','top':'10px'}}
                            type="submit"
                            value="Submit"
+                           id = "submit_button2"
                     />
                     <p id = "loading"/>
                 </form>
-
+                <div id= "area3" className={"area3"}>
+                    <p id="data"/>
+                </div>
             </div>
         )
     }
